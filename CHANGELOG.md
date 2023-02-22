caf7d3b94803c7c6d8beaa9b3263fbeb753f4ad8
Jimmy <jiamingma1998@gmail.com>
2023-02-21 22:30:17 -0500

    v1.0.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,3 +19,18 @@ Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how 
 - Added keyword "break"
 - Treat '' and "" both as strings (Not sure how Broo works)
 - Improve variable detection
+
+# [1.0.4]
+
+- Improve variable detection
+- Better function name detection
+
+# [1.0.5]
+
+- Fix single line comments
+- Fix script tag children inheriting script tag's color
+- Root of variable path will be highlighted differently (eg. "my.var.path", "my" will be colored differently)
+
+# [1.0.6]
+- < and > in tags will not be colored
+- Remove coloring of operators for better clarity (gt, ne, eg, lt, le, ge, ->, ||, ??, |, ?)
--- out/extension.js
+++ out/extension.js
@@ -6,7 +6,7 @@ exports.deactivate = exports.activate = void 0;
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "broo-language" is now active!');
+    console.log('Congratulations, your extension "broo-language" is now active!!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
