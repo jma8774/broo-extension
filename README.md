5c69e0828233cfae7cac0aa727ea89f4a7559ca5
Jimmy Ma <jiamingma1998@gmail.com>
2025-02-03 09:55:56 -0500

    Indxer, go to definition and symbol lookup

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-# Simple Syntax Highlighter for Broo
+# Language Extension for Broo
 
 
 ### How to Use
 
-Change the langugage mode to Broo on the bottom right of VSCode (should happen automatically though).
+Change the langugage mode to Broo on the bottom right of VSCode (should happen automatically).
 
 ### Where do I get it?
-
 Published to the [VSCode Marketplace](https://marketplace.visualstudio.com/items?itemName=jma8774.broo-language).
 
-### Disclaimer
+### Features
+* Syntax highlighting
+* Go to definition (Ctrl + Click or by pressing F12 on a symbol)
+  * Some things to note about the "Go to definition" implementation is that the extension has no knowledge about the hierachy of the code and the scope of where certain functions and variables may live. This is why "Go to definition" for functions and variables only searches from within the current page
+* Symbol search (Ctrl + T)
+  * All supported symbol definitions that have been indexed can be searched for with this command, allowing you to traverse the code base faster 
 
-It is not going to work 100% of the time because there will be edge cases that this simple syntax highlighter will not account for but it is good enough for me :D
+### How do I run and develope the extension locally?
+Follow the [guide](https://code.visualstudio.com/api/working-with-extensions/publishing-extension#vsce) on how to use `vsce` to generate a `.vsix` file.
+1. Run `npm run package` to generate the `.vsix` file
+2. In VSCode, under the extensions tab, install the `.vsix` extension by clicking "Install from VSIX"
+
+### Disclaimer
+It is not going to work 100% of the time because there will be edge cases that this simple syntax highlighter will not account for but it is good enough for the effort spent
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,45 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import DefinitionProvider from './providers/definition_provider';
+import SymbolProvider from './providers/workspace_symbol_provider';
+import { Indexer } from './indexer';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+	console.log('Yay, extension "broo-language" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "broo-language" is now active!!');
+	// Indexer
+	const indexer = new Indexer();
+	indexer.clear(); 
+	await indexer.indexAll();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('broo-language.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello World from Broo Language!');
-	// });
+	// Index all every 5 minutes
+	setInterval(async () => {
+		indexer.clear();
+		await indexer.indexAll();
+	}, 60000 * 5);
 
-	// context.subscriptions.push(disposable);
+	// Index specific file on change
+	vscode.workspace.onDidChangeTextDocument(async (event) => {
+    // Clear the cache or re-index affected file
+		const changedDocument = event.document;
+    await indexer.indexDocument(changedDocument);
+	});
+
+	// "Go to definition" provider (Ctrl + Click a symbol or press F12)
+	const definitionProvider = vscode.languages.registerDefinitionProvider(
+		{ language: "broo" },
+		new DefinitionProvider(indexer)
+	);
+	context.subscriptions.push(definitionProvider);
+
+	// Symbol provider (Ctrl + T to search symbols)
+	const symbolProvider = vscode.languages.registerWorkspaceSymbolProvider(
+		new SymbolProvider(indexer)
+	);
+	context.subscriptions.push(symbolProvider);
 }
 
 // This method is called when your extension is deactivated
